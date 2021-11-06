c594a1d718aec7e1ddcba77629664503954098c3
LuMel <luisomas@gmail.com>
2021-11-06 20:40:57 +0100

    added params.yml and folder src with source code

---
+++ starter/src/train_data.py
@@ -0,0 +1,62 @@
+# Script to train machine learning model.
+from sklearn import model_selection
+from prepare_data import process_data, remove_spaces
+import model
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import yaml
+import pickle
+import os
+
+def split_and_train():
+    # Add code to load in the data.
+    parameters = yaml.safe_load(open('params.yaml'))
+
+    input_path = parameters['data']['input_path']
+    data_raw = pd.read_csv(input_path + "census.csv")
+
+    # pre cleaning
+    data = remove_spaces(data_raw)
+
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, 
+                                    test_size=float(parameters['data']['train_test_split']))
+
+    cat_features = list(parameters['data']['categorical_features'])
+    
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+
+    # Proces the test data with the process_data function.
+    X_test, y_test, encoder, lb = process_data(
+        test, categorical_features=cat_features, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
+
+    Xy_train = np.concatenate([X_train, y_train.reshape(y_train.shape[0],1)], axis=1)
+    Xy_test = np.concatenate([X_test, y_test.reshape(y_test.shape[0],1)], axis=1)
+
+    os.makedirs("starter/data/prepared", exist_ok=True)
+    # save processed train/test data
+    with open("starter/data/prepared/train.pkl", 'wb') as f:
+            pickle.dump(Xy_train, f)
+
+    with open("starter/data/prepared/test.pkl", 'wb') as f:
+            pickle.dump(Xy_test, f)
+
+    depth_ = parameters['model']['max_depth']
+    imbalance_ = parameters['model']['imbalance']
+    # Train and save a model.
+    fitted_model = model.train_model(X_train, y_train, max_depth = depth_, imbalance=imbalance_)
+
+    os.makedirs("starter/model", exist_ok=True)
+    # save model to disk
+    with open('starter/model/rfc.pkl', 'wb') as file:
+        pickle.dump(fitted_model, file)
+
+    return None
+
+if __name__ == '__main__':
+    split_and_train()
