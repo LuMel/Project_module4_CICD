bcfba8e95bf7c825907eb753e170ac3cfbfc0e64
LuMel <luisomas@gmail.com>
2021-11-06 21:12:53 +0100

    changed main.py

---
+++ __init__.py
@@ -0,0 +1,3 @@
+#import sys
+
+#sys.path.append("/home/lmelgar/ml_projects/udacity_MLOps/Project_module4_CICD/starter")
---
+++ main.py
@@ -0,0 +1,4 @@
+# Put the code for your API here.
+from src.train_data import split_and_train
+
+split_and_train()
